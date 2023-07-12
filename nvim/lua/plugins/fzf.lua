f38f6ccb237e0192c25e1cdbecddd4ed56922121
Daniel Melchor Hachuel <dmelchor@stripe.com>
2023-07-12 11:40:37 -0400

    working nvim config

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,2 +1,9 @@
 vim.g.fzf_layout = { window = { width = 0.9, height = 0.6 } }
 vim.g.fzf_history_dir = '~/.local/share/fzf-history'
+
+require("fzf-lua").setup()
+
+if vim.fn.executable("fzf") ~= 1 then
+  vim.notify("fzf not found. brew install fzf", vim.log.levels.WARN)
+end
+
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,9 +3,16 @@ local lsp = require('lsp-zero').preset('recommended')
 lsp.ensure_installed({
   'tsserver',
   'lua_ls',
-  'gopls',
   'jdtls',
-  'rust_analyzer'
+  'bashls',
+  'cssls',
+  'eslint',
+  'graphql',
+  'html',
+  'jsonls',
+  'pyright',
+  'rust_analyzer',
+  'yamlls',
 })
 
 -- Preferences
@@ -25,9 +32,7 @@ lsp.setup_nvim_cmp({
     { name = 'buffer' }
   },
   mapping = {
-        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-        ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-        ['<Tab>'] = cmp.mapping(function(fallback)
+        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), ['<Tab>'] = cmp.mapping(function(fallback)
         local copilot_keys = vim.fn['copilot#Accept']()
         if cmp.visible() then
           cmp.confirm({ select = false })
@@ -58,7 +63,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set('n', '<LEADER>D', function() vim.lsp.buf.type_definition() end, opts)
   vim.keymap.set('n', '<LEADER>r', function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set('n', '<LEADER>a', function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set('n', '<LEADER>f', function() vim.lsp.buf.formatting() end, opts)
+  vim.keymap.set('n', '<LEADER>f', function() vim.lsp.buf.formatting({ async = true }) end, opts)
   vim.keymap.set('n', '<LEADER>w', function()
     vim.lsp.buf.formatting()
     vim.cmd('w')
@@ -66,21 +71,20 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
 end)
 
--- Custom configs
-local lsp_rust = lsp.build_options('rust_analyzer', {})
-lsp.configure('lua_ls', {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      }
-    }
-  }
-})
+
+-- Stripe
+require('lspconfig_stripe')
+local servers = { 'tsserver', 'gopls', 'payserver_sorbet' }
+for _, lsp in ipairs(servers) do
+  require('lspconfig')[lsp].setup({
+    on_attach = lsp.on_attach,
+    capabilities = lsp.capabilities,
+    flags = lsp.flags,
+    settings = lsp.settings,
+  })
+end
 
 lsp.setup()
-
-require('rust-tools').setup({ server = lsp_rust })
 
 vim.diagnostic.config({
   virtual_text = true,
